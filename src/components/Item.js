ea5c4eca9475dfdc29e684d01e1078e292363a54
John McKinnon <johnmckinnon23@yahoo.com>
2022-11-03 11:11:52 -0700

    commit tree 0381c8d8391035fd27d7522f72749540030eac54

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
 import React from "react";
-
+import { useState } from 'react'
 function Item({ name, category }) {
+  console.log()
+
+  const [addItem, setAddItem] = useState(false)
+
+  function addToCart() {
+    setAddItem(!addItem)
+  }
+
+  const newClass = addItem ? 'Remove From Cart' : 'Add To Cart'
+
+  const liClass = addItem ? 'in-cart' : ''
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCart} className="add">{newClass}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from 'react'
 
 function ShoppingList({ items }) {
+  console.log(items)
+
+  const [selectedCategory, setSelectedCategory] = useState([])
+
+  function applyFilter() {
+    setSelectedCategory(selectedCategory)
+    }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={applyFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
